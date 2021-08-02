4b9109780e1d886a996e246b0485e48d76d72f66
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-02 16:20:33 +0530

    Animation applied

--- app/src/main/java/com/example/frameanimation_android/MainActivity.java
+++ app/src/main/java/com/example/frameanimation_android/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.frameanimation_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable beeAnimation;
+    private ImageView beeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beeImage = (ImageView) findViewById(R.id.beeID);
+        beeImage.setBackgroundResource(R.drawable.bee_anim);
+        beeAnimation = (AnimationDrawable) beeImage.getBackground();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        beeAnimation.start();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                beeAnimation.stop();
+            }
+        }, 5100);
+
+        return super.onTouchEvent(event);
     }
 }
